fa21d0dd580bd0c2c794f5a1f8c30f17d46d7a0d
HEALER119 <147355209+HEALER-119@users.noreply.github.com>
2023-12-14 19:06:21 +0800

    Add files via upload

---
+++ function.js
@@ -0,0 +1,10 @@
+function showMenu(){
+  document.getElementById("menu").style.display = "block";
+  document.getElementById("close").style.display = "block";
+  document.getElementById("open").style.display = "none";
+}
+function hideMenu(){
+  document.getElementById("menu").style.display = "none";
+  document.getElementById("open").style.display = "block";
+  document.getElementById("close").style.display = "none";
+}
